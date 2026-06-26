7595c1ca3e83eed768f459a1baf97c9e50b21e5c
RiteshRana7 <riteshrana19819@gmail.com>
2026-06-26 15:38:02 +0530

    26/6/2026

--- day-1/script.js
+++ day-1/script.js
@@ -15,14 +15,30 @@
 // if(age < 18){
 //     console.log("you can not do vote");
 // }
-let mode = "light";
+
+
+// let mode = "light";
+// let color;
+
+// if( mode === "dark"){
+//     color ="black";
+// }
+ 
+// if(mode === "light"){
+//     color ="white";
+// }
+// console.log(color);
+
+// 
+   ////////////////////////////////
+
+let mode = "dark";
 let color;
 
-if( mode === "dark"){
-    color ="black";
+if(mode === "dark"){
+    color = "pink"; 
 }
- 
-if(mode === "light"){
-    color ="white";
+else{
+    color = "red";
 }
 console.log(color);
