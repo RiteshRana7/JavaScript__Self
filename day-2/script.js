ed2a10dd530e3a88563df03068e8d915cc570b70
RiteshRana7 <riteshrana19819@gmail.com>
2026-06-28 11:43:10 +0530

    28/6/2026

--- day-2/script.js
+++ day-2/script.js
@@ -1,18 +1,28 @@
 //  Question of if else 1.
-let score = 90;
+// let score = 90;
 
-if(score >= 90 && score <= 100){
-    console.log("A gread");
+// if(score >= 90 && score <= 100){
+//     console.log("A gread");
+// }
+// else if(score >= 70 && score <= 89){
+//      console.log("B grade");
+// }
+// else if(score >= 60 && score <= 69){
+//     console.log("C grade");
+// }
+// else if(score >= 50 && score <= 59){
+//     console.log("D grade");
+// }
+// else{
+//     console.log("Fail");
+// }
+
+//  While Loop .............2
+let i = 1;
+
+while(i <= 5){
+    console.log("i=", i);
+    i++;
 }
-else if(score >= 70 && score <= 89){
-     console.log("B grade");
-}
-else if(score >= 60 && score <= 69){
-    console.log("C grade");
-}
-else if(score >= 50 && score <= 59){
-    console.log("D grade");
-}
-else{
-    console.log("Fail");
-}
+
+
