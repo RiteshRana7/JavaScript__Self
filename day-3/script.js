1baec89b2f65815ebc529d7e88cff5c4e1e7f855
RiteshRana7 <riteshrana19819@gmail.com>
2026-07-02 12:27:08 +0530

    2/6/2026

--- day-3/script.js
+++ day-3/script.js
@@ -20,8 +20,21 @@
 
 // praxtice to upper case or lowwer case .............
 
-let str = "RiteshRana";
+// let str = "RiteshRana";
 
 
- console.log(str.toUpperCase());
-console.log(str.toLowerCase());
+//  console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
+
+
+// practice question 5...........
+
+
+let name = "KhushiRana";
+
+console.log(name.slice(0,5));
+console.log(name.slice(2));
+
+let num = "0123456789";
+
+console.log(num.slice(2,6));
