9d906021fac3fc4ec699280f38f893d2c8f8d4ed
RiteshRana7 <riteshrana19819@gmail.com>
2026-07-02 11:39:51 +0530

    2/6/2026

--- day-3/script.js
+++ day-3/script.js
@@ -6,9 +6,14 @@
 
 // ............question 2
 
-let obj ={
-    item : "pencial",
-    price : 15
-};
+// let obj ={
+//     item : "pencial",
+//     price : 15
+// };
 
-console.log("the coust of",obj.item,"is",obj.price);
+// console.log("the coust of",obj.item,"is",obj.price);
+
+let name = "Khushi Rana";
+let age = 19;
+
+console.log(`her name is ${name} and she is ${age} years old`);
