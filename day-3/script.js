112b693067d4304cd6a4cce753fb685c30d92aaa
RiteshRana7 <riteshrana19819@gmail.com>
2026-07-02 12:00:20 +0530

    2/6/2026

--- day-3/script.js
+++ day-3/script.js
@@ -13,7 +13,15 @@
 
 // console.log("the coust of",obj.item,"is",obj.price);
 
-let name = "Khushi Rana";
-let age = 19;
+// let name = "Khushi Rana";
+// let age = 19;
 
-console.log(`her name is ${name} and she is ${age} years old`);
+// console.log(`her name is ${name} and she is ${age} years old`);
+
+// praxtice to upper case or lowwer case .............
+
+let str = "RiteshRana";
+
+
+ console.log(str.toUpperCase());
+console.log(str.toLowerCase());
