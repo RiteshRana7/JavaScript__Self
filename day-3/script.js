17acffb13bf643a8370a9d2c3ea210577238966e
RiteshRana7 <riteshrana19819@gmail.com>
2026-07-02 11:23:34 +0530

    2/7/2026

--- day-3/script.js
+++ day-3/script.js
@@ -1,4 +1,14 @@
-let str = "Ritesh rana";
+// let str = "Ritesh rana";
 
-console.log(str[3]); 
-// output e
+// console.log(str[3]); 
+// // output e
+
+
+// ............question 2
+
+let obj ={
+    item : "pencial",
+    price : 15
+};
+
+console.log("the coust of",obj.item,"is",obj.price);
