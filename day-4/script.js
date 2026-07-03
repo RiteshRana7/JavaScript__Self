f5d3e3b2105a331635c0a116c80828f78a2da050
RiteshRana7 <riteshrana19819@gmail.com>
2026-07-03 16:58:11 +0530

    4/7/2026

--- day-4/script.js
+++ day-4/script.js
@@ -1,15 +1,31 @@
-// practice of array 
+ // practice of array 
 
-let name = ["Ritesh","Khushi", "Ayush","Nishuu"];
-console.log(name); 
+// let name = ["Ritesh","Khushi", "Ayush","Nishuu"];
+// console.log(name); 
 
-let marks = [73,94,78,65];
-console.log(marks);
+// let marks = [73,94,78,65];
+// console.log(marks);
 
 
-console.log (name[1]);
+// console.log (name[1]);
 
-name[2] = "Nitin";
-console.log(name);
+// name[2] = "Nitin";
+// console.log(name);
 
+
+
+// loop in array.....
+let name = ["Ritesh","Khushi", "nitin", "Kunal", "Mauuu"];
+
+for(let i = 0; i < name.length; i++){
+    console.log(name[i]);
+}
+
+q2
+
+let marks = [59,69,75,86,48,58,79,59,76,];
+
+for(let i = 0; i < marks.length; i++){
+    console.log(marks[i]);
+}
 
