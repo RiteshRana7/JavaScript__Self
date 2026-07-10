20a6096f5f2042c350e9cbf67cb4804d708f498d
RiteshRana7 <riteshrana19819@gmail.com>
2026-07-10 17:28:39 +0530

    10/7/2026

---
+++ day-4.5/script.js
@@ -0,0 +1,51 @@
+// let fruits = ["Mango","Litchi", "Grapes", "Banana","Orange"];
+
+// for(let i = 0; i < fruits.length; i++){
+//     console.log(fruits[i]);
+// }
+
+
+// let num = [10,20,30,40,50];
+
+// for(let val of num){
+//     console.log(val);
+// }
+
+// let marks = [60,65,56,59,67,53];
+
+// sum = 0;
+// for(let val of marks){
+//     sum += val;
+// }
+// average = sum / marks.length;
+// console.log(`student  average marks of ${average}`);
+
+// let fooditems = ["Litchi","Mangos","Potato","Tomato"];
+// fooditems.pop();
+// console.log(fooditems);
+// fooditems.push("Graps","Papaya","Orange");
+// console.log(fooditems);
+
+
+// let firstName = ["Ritesh","vishal","Raju","parkash"];
+// let lastName = ["Rana","Bhadwar","Singh","Bhai"];
+
+// Fullname = firstName.concat(lastName);
+// console.log(Fullname);
+
+// let firstName = ["Ritesh","vishal","Raju","parkash"];
+// firstName.unshift("Rudra","Rohit");
+// console.log(firstName);
+
+// let number = [50,60,40,60,69,59,];
+// number.unshift(10,20,30,40)
+// console.log(number);
+
+// let num = [1,2,3,4,5];
+//  value = num.slice(1,3);
+// console.log(value);
+
+let name = ["Rahul","nitin","Munna","Rohit"];
+val = name.slice(1,4);
+console.log(name);
+console.log(val);
