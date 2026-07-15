bc7d03f8a25e4f0752eaab2748d14918c56a42b6
RiteshRana7 <riteshrana19819@gmail.com>
2026-07-15 15:52:58 +0530

    15/7/2026

--- day-5/script.js
+++ day-5/script.js
@@ -1,5 +1,11 @@
-let arr = [1,2,3,4,5];
+// let arr = [1,2,3,4,5];
 
-arr.forEach(function printVal(val){
-    console.log(val);
+// arr.forEach(function printVal(val){
+//     console.log(val);
+// });
+
+
+let fruts = ["manog","apple","graps","orange","banana"];
+fruts.forEach(function frut(value){
+    console.log(value);
 });
