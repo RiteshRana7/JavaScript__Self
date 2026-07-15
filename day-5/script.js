4d544b39de084cab0e2cd09229b15397d9b44b79
RiteshRana7 <riteshrana19819@gmail.com>
2026-07-15 16:06:24 +0530

    15/7/2026

--- day-5/script.js
+++ day-5/script.js
@@ -1,3 +1,5 @@
+
+
 // let arr = [1,2,3,4,5];
 
 // arr.forEach(function printVal(val){
@@ -10,7 +12,14 @@
 //     console.log(value);
 // });
 
-let char = [10,20,30,40,50,60,70,80,90,100];
-char.forEach(function c(v){
-    console.log(v);
-});
+// let char = [10,20,30,40,50,60,70,80,90,100];
+// char.forEach(function c(v){
+//     console.log(v);
+// });
+
+// forEach in arrow function....
+
+let arr = ["pune","delhi","goa","surat","mumbai"];
+arr.forEach((val) =>{
+    console.log(val.toUpperCase());
+})
