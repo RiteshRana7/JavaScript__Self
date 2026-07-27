06f09c6c1ae105cbca788399b54fa75da2011f06
RiteshRana7 <riteshrana19819@gmail.com>
2026-07-27 15:57:40 +0530

    27/7/2026

--- day-6/script.js
+++ day-6/script.js
@@ -1,5 +1,10 @@
 // let heading = document.getElementById("heading");
 // console.log(heading);
 
-let heading = document.getElementById("heading");
-console.dir(heading);
+// let heading = document.getElementById("heading");
+// console.dir(heading);
+
+
+// let heading = document.getElementsByClassName("heading-class");
+// console.log(heading);
+// console.dir(heading)
