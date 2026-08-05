42052fe14f5829b5a5391334bd378870259b3ff5
RiteshRana7 <riteshrana19819@gmail.com>
2026-08-05 15:00:03 +0530

    5/8/2026

--- day-7/script.js
+++ day-7/script.js
@@ -1,5 +1,11 @@
-let div = document.querySelector("div");
-console.log(div);
+// let div = document.querySelector("div");
+// console.log(div);
 
-let id = div.getAttribute("id");
-console.log(id);
+// let id = div.getAttribute("id");
+// console.log(id);
+
+// let p = document.querySelector("p");
+// console.log(p);
+
+let btn = document.createElement("button");
+console.log(btn);
